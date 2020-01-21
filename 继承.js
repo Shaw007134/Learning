e8df72422ae28c1e5bc4dbb59934bbb1b3332ca2
Shaw007134 <gggshaw007134@gmail.com>
2020-01-21 09:15:00 +0800

    组合继承

--- 继承.js
+++ 继承.js
@@ -46,3 +46,28 @@ console.log(instance1.size);
 var instance2 = new SubType();
 console.log(instance2.colors);
 instance2.print();
+
+
+// 组合继承
+function SuperType(name) {
+  this.colors = ["red", "blue", "green"];
+  this.name = name;
+}
+
+SuperType.prototype.print = function () {
+  this.colors.forEach(e => console.log(e));
+}
+
+function SubType(name, size) {
+  SuperType.call(this, name);
+  this.size = size;
+}
+
+SubType.prototype = new SuperType();
+SubType.prototype.constructor = SubType;
+SubType.prototype.saySize = function () {
+  console.log(this.size);
+}
+
+var instance1 = new SubType("instance1", 100);
+var instance2 = new SubType("instance2", 50);
