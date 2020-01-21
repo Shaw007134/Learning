b3b61f46345c22c1214d04bbb77aafd6650caded
Shaw007134 <gggshaw007134@gmail.com>
2020-01-21 10:51:38 +0800

    ES6写法

--- 设计模式.js
+++ 设计模式.js
@@ -68,7 +68,7 @@ target.fire({
   message: "Hello World!"
 });
 
-function Person(name, age){
+function Person(name, age) {
   EventTarget.call(this);
   this.name = name;
   this.age = age;
@@ -76,8 +76,11 @@ function Person(name, age){
 
 Person.prototype = new EventTarget();
 Person.prototype.constructor = Person;
-Person.prototype.say = function(message) {
-  this.fire({type: "message", message: message});
+Person.prototype.say = function (message) {
+  this.fire({
+    type: "message",
+    message: message
+  });
 };
 
 function handleMessage(event) {
@@ -90,3 +93,50 @@ var person = new Person("zsy", 25);
 person.addHandler("message", handleMessage);
 // 在该对象上调用1个方法，它触发消息事件
 person.say("Happy new year 2020")
+
+
+// ES6 写法
+// 主题，接收状态变化，触发每个观察者
+class Subject {
+  constructor() {
+    this.state = 0
+    this.observers = []
+  }
+  getState() {
+    return this.state
+  }
+  setState(state) {
+    this.state = state
+    this.notifyAllObservers()
+  }
+  attach(observer) {
+    this.observers.push(observer)
+  }
+  notifyAllObservers() {
+    this.observers.forEach(observer => {
+      observer.update()
+    })
+  }
+}
+
+// 观察者，等待被触发
+class Observer {
+  constructor(name, subject) {
+    this.name = name
+    this.subject = subject
+    this.subject.attach(this)
+  }
+  update() {
+    console.log(`${this.name} update, state: ${this.subject.getState()}`)
+  }
+}
+
+// 测试代码
+let s = new Subject()
+let o1 = new Observer('o1', s)
+let o2 = new Observer('o2', s)
+let o3 = new Observer('o3', s)
+
+s.setState(1)
+s.setState(2)
+s.setState(3)
